d3efa052c6aacc260c02efba79ffc35147a6f5d7
Jonathan Lopez <jlopez0313@hotmail.com>
2024-05-17 17:09:56 -0500

    fix users

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,10 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->withMiddleware(function (\Middleware $middleware) {
+    $middleware->group('universal', []);
+});
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
--- routes/auth.php
+++ routes/auth.php
@@ -1,5 +1,8 @@
 <?php
 
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
+
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
@@ -11,7 +14,11 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
+Route::middleware([
+    'guest',
+    'universal',
+    InitializeTenancyByDomain::class
+])->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
@@ -35,7 +42,12 @@ Route::middleware('guest')->group(function () {
                 ->name('password.store');
 });
 
-Route::middleware('auth')->group(function () {
+
+Route::middleware([
+    'auth',
+    'universal',
+    InitializeTenancyByDomain::class
+])->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
                 ->name('verification.notice');
 
