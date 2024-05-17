a78ea5cdcebcc1537dc9771dc2f8376700b4425d
Jonathan Lopez <jlopez0313@hotmail.com>
2024-05-17 17:25:25 -0500

    fix users

--- routes/auth.php
+++ routes/auth.php
@@ -1,8 +1,5 @@
 <?php
 
-use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
-use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
-
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
@@ -14,11 +11,7 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([
-    'guest',
-    'universal',
-    InitializeTenancyByDomain::class
-])->group(function () {
+Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
@@ -42,12 +35,7 @@ Route::middleware([
                 ->name('password.store');
 });
 
-
-Route::middleware([
-    'auth',
-    'universal',
-    InitializeTenancyByDomain::class
-])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
                 ->name('verification.notice');
 
