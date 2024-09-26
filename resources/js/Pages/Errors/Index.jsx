d5352aa9a1905a02d076a475a54b9f9aea9396cd
Jonathan Lopez <jlopez0313@hotmail.com>
2024-09-26 09:50:57 -0500

    fecha limite pago

--- resources/js/Pages/Errors/Index.jsx
+++ resources/js/Pages/Errors/Index.jsx
@@ -7,7 +7,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, router } from "@inertiajs/react";
 import styles from "./Errors.module.css";
 
-export default ({ auth, error }) => {
+export default ({ auth, error, ...props }) => {
     const LazyComponent = React.lazy(() => import(error));
 
     return (
